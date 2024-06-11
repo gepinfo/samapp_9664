d02569ad27ac08045c1ff793be4fef996f966f47
gepinfo <gepinfo>
2024-06-11 12:11:28 +0000

    updated generated code

--- devops/cloud/aws/github_actions/workflows/task-definition.ts
+++ devops/cloud/aws/github_actions/workflows/task-definition.ts
@@ -70,7 +70,8 @@ module.exports = {
             ],
             "environmentFiles": null,
             "environment": [{"name":"MONGO_DB_URL","value":"mongodb://admin:password@127.0.0.1:27017/samapp_9664?authSource=admin"},{"name":"MONGO_DOMAIN","value":"127.0.0.1"},{"name":"SECURITYURL","value":"http://127.0.0.1:8003"},{"name":"AUTHPROXYURL","value":"http://127.0.0.1:8001"},{"name":"ADMINURL","value":"http://127.0.0.1:8004"},{"name":"GCAMURL","value":"http://127.0.0.1:8007"},{"name":"APIGATEWAY","value":"http://127.0.0.1:8000"},{"name":"SYSTEMCREDENTIALMANAGERURL","value":"http://127.0.0.1:8005"}
-            ,{"name":"CRUDAPPURL","value":"http://samapp.local:8135"}
+            ,{"name":"SYSTEMENTRYURL","value":"http://127.0.0.1:8137"}
+            ,{"name":"CRUDAPPURL","value":"http://samapp0.local:8137"}
             ],
             "secrets": null,
             "mountPoints": [
@@ -292,6 +293,7 @@ module.exports = {
             ],
             "environmentFiles": null,
             "environment": [{"name":"MONGO_DB_URL","value":"mongodb://admin:password@127.0.0.1:27017/samapp_9664?authSource=admin"},{"name":"MONGO_DOMAIN","value":"127.0.0.1"},{"name":"SECURITYURL","value":"http://127.0.0.1:8003"},{"name":"AUTHPROXYURL","value":"http://127.0.0.1:8001"},{"name":"ADMINURL","value":"http://127.0.0.1:8004"},{"name":"GCAMURL","value":"http://127.0.0.1:8007"},{"name":"APIGATEWAY","value":"http://127.0.0.1:8000"}
+            ,{"name":"SYSTEMENTRYURL","value":"http://127.0.0.1:8137"}
             ],
             "mountPoints": [
                 {
@@ -365,16 +367,16 @@ module.exports = {
             }
         },
         {
-            "name": "crudapp-9664",
-            "image": `${process.env.AWS_ACCOUNT_ID}.dkr.ecr.us-east-1.amazonaws.com/geppetto-generatedcode-samapp-9664-crudapp:latest`,
+            "name": "systementry-9664",
+            "image": `${process.env.AWS_ACCOUNT_ID}.dkr.ecr.us-east-1.amazonaws.com/geppetto-generatedcode-samapp-9664-systementry:latest`,
             "memoryReservation": "300",
             "resourceRequirements": null,
             "essential": true,
             "portMappings": [
                 {
-                    "containerPort": "8135",
+                    "containerPort": "8137",
                     "protocol": "tcp",
-                    "hostPort": "8135"
+                    "hostPort": "8137"
                 }
             ],
             "environmentFiles": null,
---
+++ devops/cloud/aws/github_actions/workflows/task-definition_0.ts
@@ -0,0 +1,85 @@
+module.exports = {
+    requiresCompatibilities: [
+        "FARGATE"
+    ],
+    "inferenceAccelerators": [],
+    "containerDefinitions": [
+        {
+            "name": "crudapp-9664",
+            "image": `${process.env.AWS_ACCOUNT_ID}.dkr.ecr.us-east-1.amazonaws.com/geppetto-generatedcode-samapp-9664-crudapp:latest`,
+            "memoryReservation": "300",
+            "resourceRequirements": null,
+            "essential": true,
+            "portMappings": [
+                {
+                    "containerPort": "8137",
+                    "protocol": "tcp",
+                    "hostPort": "8137"
+                }
+            ],
+            "environmentFiles": null,
+            "environment": [{"name":"MONGO_DB_URL","value":"mongodb://admin:password@samapp.local:27017/samapp_9664?authSource=admin"},{"name":"MONGO_DOMAIN","value":"samapp.local"},{"name":"CAMUNDAPOD_URL","value":"http://samapp.local:8080"},{"name":"SECURITYURL","value":"http://samapp.local:8003"},{"name":"AUTHPROXYURL","value":"http://samapp.local:8001"},{"name":"ADMINURL","value":"http://samapp.local:8004"},{"name":"CAMUNDAURL","value":"http://samapp.local:8002"},{"name":"GCAMURL","value":"http://samapp.local:8007"},{"name":"APIGATEWAY","value":"http://samapp.local:8000"}],
+            "secrets": null,
+            "mountPoints": [
+                {
+                  "readOnly": null,
+                  "containerPath": "/path/to/container",
+                  "sourceVolume": "samapp"
+                }
+              ],
+            "volumesFrom": null,
+            "hostname": null,
+            "user": null,
+            "workingDirectory": null,
+            "extraHosts": null,
+            "logConfiguration": {
+                "logDriver": "awslogs",
+                "options": {
+                    "awslogs-group": "/ecs/samapp",
+                    "awslogs-region": "us-east-1",
+                    "awslogs-stream-prefix": "ecs",
+                    "awslogs-create-group": "true"
+                }
+            },
+            "ulimits": null,
+            "dockerLabels": null,
+            "dependsOn": null,
+            "repositoryCredentials": {
+                "credentialsParameter": ""
+            }
+        },          
+    ],
+    "volumes": [
+        {
+        "fsxWindowsFileServerVolumeConfiguration": null,
+        "efsVolumeConfiguration": {
+            "transitEncryptionPort": null,
+            "fileSystemId": `${process.env.EFS_ID}`,
+            "authorizationConfig": {
+            "iam": "DISABLED",
+            "accessPointId": null
+            },
+            "transitEncryption": "DISABLED",
+            "rootDirectory": "/"
+        },
+        "name": "samapp",
+        "host": null,
+        "dockerVolumeConfiguration": null
+        }
+    ],
+    "networkMode": "awsvpc",
+    "memory": "4096",
+    "cpu": "1024",
+    "executionRoleArn": `arn:aws:iam::${process.env.AWS_ACCOUNT_ID}:role/${process.env.AWS_ROLE_FOR_FARGATE}`,
+    "family": "samapp0",
+    "tags": [],
+    "placementConstraints": [],
+    "taskRoleArn": `arn:aws:iam::${process.env.AWS_ACCOUNT_ID}:role/${process.env.AWS_ROLE_FOR_FARGATE}`
+
+};
+var ConfigurationJSON = JSON.stringify(module.exports);
+const config = module.exports;
+
+const jsonConfig = JSON.parse(JSON.stringify(ConfigurationJSON))
+
+console.log(jsonConfig)
